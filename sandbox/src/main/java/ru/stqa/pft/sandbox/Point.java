d698879fa70e607ecdf9e8f6642bd0bab3d07faa
Dmitry029 <kdp_029@tut.by>
2017-01-24 20:39:19 +0300

    Сделана функция выч расстояния между объектамию

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Администратор on 24.01.2017.
+ */
+public class Point {
+  public double x;
+  public double y;
+
+  public Point(double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+
+}
