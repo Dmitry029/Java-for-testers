5c55ede1add2529dec959a6bf4aeeb11f78c0773
Dmitry029 <kdp_029@tut.by>
2017-02-10 10:11:59 +0300

    со вчера не сохр

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,21 +4,24 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
+import java.util.List;
+
 public class ContactCreationTests extends TestBase {
 
   @Test
   public void testContactCreation() {
-    //Подсчет кол-ва контактов до создания нового
-    app.getNavigationHelper().gotoHomePage();
-    int before = app.getContactHelper().getContactCount();
-    //блок подсчета окончен
+    //Подсчет кол-ва контактов до создания нового**************************************************
+    app.getNavigationHelper().gotoHomePage(); //Переход на домашнюю страницу***********************
+    List<ContactData> before = app.getContactHelper().getContactList(); //before - список объектов*
+    //блок подсчета окончен************************************************************************
     app.getNavigationHelper().gotoAddNewContact();       // Переход на стр создания контакта
-    app.getContactHelper().createContact(new ContactData("Ivan16", "Pomidorov",
+    app.getContactHelper().createContact(new ContactData("Ivan15", "Pomidorov",
             "Minsk, Gagarina 21/14", "+375 17 5544120",
             "+375 29 6222552", "test8"), true);
-    int after = app.getContactHelper().getContactCount(); //Подсчет кол-ва контактов после создания
-    Assert.assertEquals(after, before + 1);      // Сравнение результатов до и после создания
+    List<ContactData> after = app.getContactHelper().getContactList(); //after - список объектов после добавления
+    Assert.assertEquals(after.size(), before.size() + 1);      // Сравнение результатов до и после создания
   }
 }
 
-
+//int after = app.getContactHelper().getContactCount(); //Подсчет кол-ва контактов после создания
+//int before = app.getContactHelper().getContactCount(); // старая реализация
