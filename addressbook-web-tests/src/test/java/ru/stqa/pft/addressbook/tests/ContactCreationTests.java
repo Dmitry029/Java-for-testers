719b600f45b7452dd0bd32a6f0bd8e0e657c086c
Dmitry029 <kdp_029@tut.by>
2017-02-10 22:38:53 +0300

    Дз 10 готово

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -27,11 +28,16 @@ public class ContactCreationTests extends TestBase {
     List<ContactData> after = app.getContactHelper().getContactList(); //after - список объектов после добавления
     Assert.assertEquals(after.size(), before.size() + 1);     // Сравнение результатов
 
-    //ищем элемент с максимальный идентификатор. это - идентификатор новой группы l4_m9
-    contact.setId(after.stream().max((o1,o2)-> Integer.compare(o1.getId(), o2.getId())).get().getId());
-
     before.add(contact); // та же локальн пер (чтобы не писать два раза)
-    Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));//сравнение 2-х МНОЖЕСТВ
+
+    Comparator<? super ContactData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
+    before.sort(byId);
+    after.sort(byId);
+    Assert.assertEquals(before,after);//сравнение 2-х Спиcков упроядоченных по собственным правилам
   }
 }
 
+//ищем элемент с максимальный идентификатор. это - идентификатор новой группы l4_m9
+// contact.setId(after.stream().max((o1,o2)-> Integer.compare(o1.getId(), o2.getId())).get().getId());
+// before.add(contact); // та же локальн пер (чтобы не писать два раза)
+// Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));//сравнение 2-х МНОЖЕСТВ
