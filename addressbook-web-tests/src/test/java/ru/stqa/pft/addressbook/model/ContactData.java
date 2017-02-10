e094f13ddd5dba0908283e4700765b39ca500672
Dmitry029 <kdp_029@tut.by>
2017-02-10 11:04:50 +0300

    Удаление контактов сделано

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -27,24 +27,44 @@ public class ContactData {
   public String getFirstname() {
     return firstname;
   }
-
-  public String getLastname() {
-    return lastname;
-  }
-
+  public String getLastname() { return lastname;  }
   public String getAddress() {
     return address;
   }
-
   public String getHomephone() {
     return homephone;
   }
-
   public String getMobilephone() {
     return mobilephone;
   }
+  public String getGroup() { return group;  }
 
-  public String getGroup() {    return group;  }
+
+  @Override //Метод проверки сравнения элементов списков l4_m6
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstname != null ? firstname.hashCode() : 0;
+    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+    return result;
+  }
+
+  @Override //сообщеничя об ошибках l4_m6
+  public String toString() {
+    return "ContactData{" +
+            "firstname='" + firstname + '\'' +
+            ", lastname='" + lastname + '\'' +
+            '}';
+  }
 }
 
 
