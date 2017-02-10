7054718cfc37d395dccd8ab4274a4af7ee43ff05
Dmitry029 <kdp_029@tut.by>
2017-02-10 21:46:28 +0300

    GroupCreation и лямбда функция

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -16,20 +16,14 @@ public class GroupCreationTests extends TestBase{
     app.getNavigationHelper().gotoGtoupPage();     //Переход на стр Groups
     List<GroupData> before = app. getGroupHelper(). getGroupList(); //Подсчет групп до добавления
 
-    GroupData group = new GroupData("test8", null, null);
+    GroupData group = new GroupData("test2", null, null);
 
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app. getGroupHelper(). getGroupList();  //Подсчет групп после добавления
     Assert.assertEquals(after.size(), before.size() + 1);   // Проверка кол-ва групп до и после создания
 
-    //ищем максимальный идентификатор. Он - идентификатор новой группы
-    int max =0;
-    for (GroupData g : after) {
-      if (g.getId() > max){
-      max = g.getId();
-      }
-    }
-    group.setId(max);
+    //ищем элемент с максимальный идентификатор. это - идентификатор новой группы
+    group.setId(after.stream().max((o1,o2)-> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));//сравнение 2-х МНОЖЕСТВ
   }
