39d84c38c64a463ab1ffde91871d6e6a98da166c
Dmitry029 <kdp_029@tut.by>
2017-02-14 20:46:40 +0300

    Групп креатион модифицирован(проверки)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
@@ -8,24 +10,28 @@ import ru.stqa.pft.addressbook.model.GroupData;
 import java.util.Comparator;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
+
 
 public class GroupCreationTests extends TestBase{
 
   @Test
   public void testGroupCreationTests() {
-    app.goTo().groupPage();     //Переход на стр Groups
+    app.goTo().groupPage();
     List<GroupData> before = app.group().list(); //Подсчет групп до добавления
     GroupData group = new GroupData().withName("test2");//данные для создания группы
     app.group().create(group);//создаем группу
     List<GroupData> after = app.group().list();  //Подсчет групп после добавления
-    Assert.assertEquals(after.size(), before.size() + 1);   // Проверка кол-ва групп до и после создания
+    assertThat(after.size(), equalTo(before.size() + 1));   // Проверка кол-ва групп до и после создания
 
     before.add(group);
     //упорядочиваем списки (мах идентификатор искать не будем  l4_m10
     Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
     before.sort(byId);
     after.sort(byId);
-    Assert.assertEquals(before,after);//сравнение 2-х Списков упроядоченных по собственным правилам
+    assertThat(after, equalTo(before));
     }
 }
 
