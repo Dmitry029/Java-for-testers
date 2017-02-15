6feed35c9173c06767f7494077a02387c181e6df
Dmitry029 <kdp_029@tut.by>
2017-02-15 14:06:42 +0300

    GroupDelation модифицированна

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,10 +1,18 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
 import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupDeletionTests extends TestBase{
     @BeforeMethod  //Перед каждым вызовом   - проверка предусловий i5_m2
@@ -21,13 +29,11 @@ public class GroupDeletionTests extends TestBase{
     @Test
     public void testGroupDeletion() {
 
-        Set<GroupData> before = app.group().all(); //Подсчет групп  до удаления
+        Groups before = app.group().all(); //Подсчет групп  до удаления
         GroupData deletedGroup = before.iterator().next();
         app.group().delete(deletedGroup);// для удаления передаем группу целиком ВЫБРАНА СЛУЧАЙНЫМ ОБРАЗОМ
-        Set<GroupData> after = app.group().all();  //Подсчет групп после удаления
-        Assert.assertEquals(after.size(), before.size() - 1);  // Проверка кол-ва групп до и после удаления    }
-
-        before.remove(deletedGroup);
-        Assert.assertEquals(before, after);  //сравниваем по элементно списки
-     }
+        Groups after = app.group().all();  //Подсчет групп после удаления
+        assertEquals(after.size(), before.size() - 1);  // Проверка кол-ва групп до и после удаления    }
+        assertThat(after, equalTo(before.without(deletedGroup)));
+    }
 }
