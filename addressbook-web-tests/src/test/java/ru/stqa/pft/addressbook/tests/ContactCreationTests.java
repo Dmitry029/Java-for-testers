8099444b54a0917d6e81e73b596bb1763f679cf4
Dmitry029 <kdp_029@tut.by>
2017-02-22 12:02:00 +0300

    Загрузка тестовых данных группы и контакты реализована

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -12,7 +12,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -21,23 +21,22 @@ import java.util.Set;
 public class ContactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() {
-    List<Object[]> list = new ArrayList<Object[]>();
-    File photo = new File("src/test/resources/images.png");//относительный путь к файлу.
-    // Относительный по отношению к рабочей директории!! Т к программ на разных комп нах в разных
-    // рабочих директориях  -АБСОЛЮТНЫЙ путь указывать нельзя!!!
-    list.add(new Object[] {new ContactData().withFirstname("Sasha1").withLastname("Rovny1").withAddress("Gogola 1")
-            .withHomePhone("+375 671").withMobilePhone("+375 781").withWorkPhone("+375 001").withEmail("qqq1")
-            .withEmail2("www1").withEmail3("eee1").withPhoto(photo)});
-    list.add(new Object[] {new ContactData().withFirstname("Sasha2").withLastname("Rovny2").withAddress("Gogola 2")
-            .withHomePhone("+375 672").withMobilePhone("+375 782").withWorkPhone("+375 002").withEmail("qqq2")
-            .withEmail2("www2").withEmail3("eee2").withPhoto(photo)});
-    list.add(new Object[] {new ContactData().withFirstname("Sasha3").withLastname("Rovny3").withAddress("Gogola 3")
-            .withHomePhone("+375 673").withMobilePhone("+375 783").withWorkPhone("+375 003").withEmail("qqq3")
-            .withEmail2("www3").withEmail3("eee3").withPhoto(photo)});
-    return  list.iterator();
-  }
+  public Iterator<Object[]> validContacts() throws IOException {
 
+      List<Object[]> list = new ArrayList<Object[]>();
+      BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+      String line = reader.readLine();
+      while(line != null){
+        String[] split = line.split(";");
+        list.add(new Object[] {new ContactData().withFirstname(split[0]).withLastname(split[1]).withAddress(split[2])
+                  .withHomePhone(split[3]).withMobilePhone(split[4]).withWorkPhone(split[5]).withEmail(split[6])
+                  .withEmail2(split[7]).withEmail3(split[8])});
+        line = reader.readLine();
+      }
+      return  list.iterator();
+    }
+
+//  list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
 
   @Test(dataProvider = "validContacts")                           //    (enabled = false) //выкл теста
   public void testContactCreation(ContactData contact) {
@@ -72,3 +71,15 @@ public class ContactCreationTests extends TestBase {
   }*/
 
 
+/*File photo = new File("src/test/resources/images.png");//относительный путь к файлу.
+    // Относительный по отношению к рабочей директории!! Т к программ на разных комп нах в разных
+    // рабочих директориях  -АБСОЛЮТНЫЙ путь указывать нельзя!!!
+    list.add(new Object[] {new ContactData().withFirstname("Sasha1").withLastname("Rovny1").withAddress("Gogola 1")
+            .withHomePhone("+375 671").withMobilePhone("+375 781").withWorkPhone("+375 001").withEmail("qqq1")
+            .withEmail2("www1").withEmail3("eee1").withPhoto(photo)});
+    list.add(new Object[] {new ContactData().withFirstname("Sasha2").withLastname("Rovny2").withAddress("Gogola 2")
+            .withHomePhone("+375 672").withMobilePhone("+375 782").withWorkPhone("+375 002").withEmail("qqq2")
+            .withEmail2("www2").withEmail3("eee2").withPhoto(photo)});
+    list.add(new Object[] {new ContactData().withFirstname("Sasha3").withLastname("Rovny3").withAddress("Gogola 3")
+            .withHomePhone("+375 673").withMobilePhone("+375 783").withWorkPhone("+375 003").withEmail("qqq3")
+            .withEmail2("www3").withEmail3("eee3").withPhoto(photo)});*/
