a52514ec303a77b96046759448a2b97613d79e81
Dmitry029 <kdp_029@tut.by>
2017-02-22 19:27:27 +0300

    Автоматическое закрывание файлов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -8,8 +8,7 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
-import ru.stqa.pft.addressbook.model.GroupData;
-import ru.stqa.pft.addressbook.model.Groups;
+
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -28,34 +27,37 @@ public class ContactCreationTests extends TestBase {
   public Iterator<Object[]> validContactsFromXml() throws IOException {
 
     List<Object[]> list = new ArrayList<Object[]>();
-       BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")));
-       String xml = "";
-       String line = reader.readLine();
-       while(line != null){
-           xml += line;
-           line = reader.readLine();
-       }
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")))) {
+      String xml = "";
+      String line = reader.readLine();
+      while (line != null) {
+        xml += line;
+        line = reader.readLine();
+      }
 
-       XStream xstream = new XStream();
-       xstream.processAnnotations(ContactData.class);
-       List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
-       return contacts.stream().map((g)-> new Object[] {g}).collect(Collectors.toList()).iterator();
+      XStream xstream = new XStream();
+      xstream.processAnnotations(ContactData.class);
+      List<ContactData> contacts = (List<ContactData>) xstream.fromXML(xml);
+      return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
+  }
 
   @DataProvider
   public Iterator<Object[]> validContactsFromJson() throws IOException {
 
     List<Object[]> list = new ArrayList<Object[]>();
-    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")));
-    String json = "";
-    String line = reader.readLine();
-    while(line != null){
-      json += line;
-      line = reader.readLine();
+    try (BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))) {
+      String json = "";
+      String line = reader.readLine();
+      while (line != null) {
+        json += line;
+        line = reader.readLine();
+      }
+      Gson gson = new Gson();
+      List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>() {
+      }.getType());
+      return contacts.stream().map((g) -> new Object[]{g}).collect(Collectors.toList()).iterator();
     }
-    Gson gson = new Gson();
-    List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
-    return contacts.stream().map((g)-> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
   @Test(dataProvider = "validContactsFromJson")                           //    (enabled = false) //выкл теста
